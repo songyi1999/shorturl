48e70be68142f49b9a5b936e764afc8d9feee4a0
songyi1999 <452350370@qq.com>
2017-04-18 23:32:13 +0800

    add code

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var app = require("express")();
+var mongo = require("mongodb").MongoClient;
+var db = 'mongodb://localhost:27017/shorturl';
 
-add.get("/:site", function(req, res) {
+app.get('/', function(req, res) {
+    res.end("short url server,input a site url append this site url,then return a short url");
+})
+app.get('/:site', function(req, res) {
     var site = req.params.site;
-    res.end(site);
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+    if (site.match(regex)) {
+        var website = db.collection('website');
+
+
+
+    } else {
+        res.end("Not a website");
+    }
+
+    res.end(JSON.stringify(siteurl));
 
 
 })
